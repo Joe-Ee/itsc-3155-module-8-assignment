68d2f9d6bde6e17d3a720cfbdc0eecbca7d764fa
Joe_Ee <joeyskimboard@gmail.com>
2022-03-14 23:47:35 -0400

    changing things

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, redirect, render_template
-from src.repositories.movie_repository import movie_repository_singleton
+from flask import Flask, redirect, render_template, request, abort, url_for
+from src.repositories.movie_repository import MovieRepository, movie_repository_singleton
+from src.models.movie import Movie
+
 
 app = Flask(__name__)
 
@@ -30,4 +32,19 @@ def create_movie():
 @app.get('/movies/search')
 def search_movies():
     # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
+
+    
+        #store whatever the user searched for in the title
+    title = request.args.get('searchMov', "-2")
+
+            #if they did search for something get the movie
+    movie = movie_repository_singleton.get_movie_by_title(title)
+    #if nothing was put into the search bar create a fake movie that notifies the program that nothing was serached for
+    if title =="" or title == "-2":
+        movie = Movie("-1", "-1", -1)
+    
+        
+#code I wrote end here
+
+    return render_template('search_movies.html', search_active=True, movie = movie, title = title)
+#
