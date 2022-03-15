caa7c2b19901066fed2935f286b01f5c53e96c9b
JMAbel-hub <72168297+JMAbel-hub@users.noreply.github.com>
2022-03-15 00:21:02 -0400

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from src.repositories.movie_repository import movie_repository_singleton
 
 app = Flask(__name__)
@@ -22,9 +22,20 @@ def create_movies_form():
 
 @app.post('/movies')
 def create_movie():
-    # TODO: Feature 2
-    # After creating the movie in the database, we redirect to the list all movies page
-    return redirect('/movies')
+    # TODO: Feature 2 Jeremy Abel
+    title = request.form.get('title', None)
+    director = request.form.get('director', None)
+    rating = request.form.get('rating', 0)
+
+    # Input verification 
+    if (title==None or director==None or rating<1 or rating>5):
+        # Creates a new form 
+        return redirect('/movies/new')
+    else:
+        # Adds the movie review to the list of reviews 
+        movie_repository_singleton.create_movie(title, director, rating)
+        # After creating the movie in the database, we redirect to the list all movies page
+        return redirect('/movies')
 
 
 @app.get('/movies/search')
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,14 @@
-# TODO: Feature 2
+# TODO: Feature 2 || Jeremy Abel
+from src.repositories.movie_repository import movie_repository_singleton
+from src.models.movie import Movie
+
+def test_create_movie():
+    testMovie = movie_repository_singleton.create_movie('Star Wars', 'George Lucas', 5)
+    
+    assert type(testMovie) == Movie
+    assert testMovie.title == 'Star Wars'
+    assert testMovie.director == 'George Lucas'
+    assert testMovie.rating == 5
+    
+    
+
